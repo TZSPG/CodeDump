3b01e790b12639a1e63e16a41104a7f1f9d93a01
Tynan Jones <tcj11@hotmail.com.au>
2017-07-21 20:10:40 +1000

    Refactored old code

--- code/Guess the number.py
+++ code/Guess the number.py
@@ -1,19 +1,22 @@
-#Guess the number
+from random import randint
 
-import random
-num = random.randint(0,10)
 
-def Guessing(x):
-        try:
-                if int(x) > num:
-                        Guessing(int(input('Go lower: ')))
-                elif int(x) < num:
-                         Guessing(int(input('Go higher: ')))
-                else:
-                        print('Correct!')
-                
-        except ValueError:
-                Guessing(int(input('That\'s not a number!: ')))
+def guessing_game(guess, highest):
+    try:
+        guess = int(guess)
+    except ValueError:
+        guessing_game(input("That's not a number!: "), highest)
+    else:
+        if guess > answer:
+            guessing_game(input('Go lower: '), highest)
+        elif guess < answer:
+            guessing_game(input('Go higher: '), highest)
+        else:
+            print('Correct!')
 
-guess = input('Enter a number between 1 and 10: ')
-Guessing(guess)
+
+if __name__ == '__main__':
+    maximum = int(input('Enter a maximum number: '))
+    answer = randint(1, maximum)
+    first_guess = input('\nEnter a number between 1 and {}: '.format(maximum))
+    guessing_game(first_guess, maximum)
--- code/Linear Equation Finder.py
+++ code/Linear Equation Finder.py
@@ -1,22 +1,24 @@
+def equation(point_a, point_b):
+    x1 = int(string_handler(point_a)[0])
+    y1 = int(string_handler(point_a)[1])
+    x2 = int(string_handler(point_b)[0])
+    y2 = int(string_handler(point_b)[1])
+    try:
+        grad = gradient(x1, x2, y1, y2)
+        intersect = y1 - grad * x1
+        print('\n' + 'The equation of the line is y = {}x + {}'.format(grad, intersect))
+    except ZeroDivisionError:
+        print('\n' + 'The equation of the line is x = {}'.format(x1))
+
+
 def gradient(x1, x2, y1, y2):
     deltax = abs(x2 - x1)
     deltay = abs(y2 - y1)
     return deltay / deltax
 
-def equation(pointA, pointB):
-    x1 = int(string_handler(pointA)[0])
-    y1 = int(string_handler(pointA)[1])
-    x2 = int(string_handler(pointB)[0])
-    y2 = int(string_handler(pointB)[1])
-    try:
-        grad = gradient(x1, x2, y1, y2)
-        intersect = y1 - grad * x1
-        print('The equation of the line is y = {}x + {}'.format(grad, intersect))
-    except ZeroDivisionError:
-        print('The equation of the line is x = {}'.format(x1))
 
 def string_handler(coordinate):
-    # Coordinate is in string format (x, y), with or without spaces
+    """Coordinate is in string format (x, y), with or without spaces"""
     if coordinate[0] == '(' and coordinate[-1] == ')':
         coordinate = coordinate[1:-1]
     coordinate = coordinate.split(',')
@@ -25,7 +27,7 @@ def string_handler(coordinate):
     return x, y
 
 
-point1 = input('Enter a coordinate in the format (x, y):  ')
-point2 = input('Enter another coordinate in the format (x, y):  ')
-
-equation(point1, point2)
+if __name__ == '__main__':
+    point1 = input('Enter a coordinate in the format (x, y):  ')
+    point2 = input('Enter another coordinate in the format (x, y):  ')
+    equation(point1, point2)
