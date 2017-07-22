8c54a8d9aa2d2ffc120c63c324a1269b2fca6e43
Tynan Jones <tcj11@hotmail.com.au>
2017-07-22 14:59:28 +1000

    Began separating guess function from file, in case of import

--- code/Guess the number.py
+++ code/Guess the number.py
@@ -1,22 +1,23 @@
 from random import randint
 
+MAXIMUM = int(input('Enter a maximum number: '))
+ANSWER = randint(1, MAXIMUM)
 
-def guessing_game(guess, highest):
+
+def guessing_game(guess, correct=ANSWER):
     try:
         guess = int(guess)
     except ValueError:
-        guessing_game(input("That's not a number!: "), highest)
+        guessing_game(input("That's not a number!: "), correct)
     else:
-        if guess > answer:
-            guessing_game(input('Go lower: '), highest)
-        elif guess < answer:
-            guessing_game(input('Go higher: '), highest)
+        if guess > correct:
+            guessing_game(input('Go lower: '), correct)
+        elif guess < correct:
+            guessing_game(input('Go higher: '), correct)
         else:
             print('Correct!')
 
 
 if __name__ == '__main__':
-    maximum = int(input('Enter a maximum number: '))
-    answer = randint(1, maximum)
-    first_guess = input('\nEnter a number between 1 and {}: '.format(maximum))
-    guessing_game(first_guess, maximum)
+    first_guess = input('\nEnter a number between 1 and {}: '.format(MAXIMUM))
+    guessing_game(first_guess)
