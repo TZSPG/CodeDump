34d867267940f9466d4f787058a874c397e996ff
Tynan Jones <tcj11@hotmail.com.au>
2017-07-24 17:18:55 +1000

    File takes user input, and now returns None if number is prime

--- code/Factor Finder.py
+++ code/Factor Finder.py
@@ -1,11 +1,18 @@
 def find_factors(num):
-    """Returns sorted list of the factors of an input"""
+    """Returns sorted list of the factors of an input. Returns None if number is prime"""
     factors = []
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             print('{} and {} are factors of {}'.format(i, num // i, num))
             factors.extend([i, num // i])
-    print('\n' + 'All factors found.')
-    return sorted(factors)
+    if len(factors) == 0:
+        print('\nThis number is prime')
+        return None
+    else:
+        print('\n' + '{} factors found. Sorting list...'.format(len(factors)))
+        return sorted(factors)
 
-print(find_factors(60))
+
+if __name__ == '__main__':
+    user_num = int(input('What number do you want the factors of: '))
+    print(find_factors(user_num))
