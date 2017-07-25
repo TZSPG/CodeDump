e73381fc1d4cddbb937cd832a9f62b0df379ac72
Tynan Jones <tcj11@hotmail.com.au>
2017-07-25 12:01:09 +1000

    Fleshed out encrypt function, and created decrypt

--- code/Great Fantastic File Encryption.py
+++ code/Great Fantastic File Encryption.py
@@ -5,29 +5,43 @@ def unicode_index(letter):
 
 def caesar(line, shift):
     """Applies caesar shift to line"""
-    for letter in line:
-        pass
-
-
-
-def encrypt(line):
-    # TODO make unicode function start at the index of the line itself
-    for letter in line[:10]:
-        caesar_shift = unicode_index(letter)
-        if caesar_shift is not None:
-            break
-    # TODO Think about abstracting to function
-
-
-
-def decrypt():
-    pass
+    return line[shift:] + line[:shift]
 
 
 def file_handler():
     pass
 
+def encrypt(line):
+    # TODO make unicode function start at the index of the line itself
+    caesar_shift = 1
+    # for letter in line[:10]:
+    #     caesar_shift = unicode_index(letter)
+    #     if caesar_shift is not None:
+    #         break
+    # TODO Think about abstracting to function?
 
-test_line = "This is a test. Please don't hurt me!"
-# print(unicode_index('A'))
-encrypt('123l')
+    shifted = caesar(line, caesar_shift)
+    unicoded = ' '.join(str(ord(letter) + index) for index, letter in enumerate(shifted))
+
+    return unicoded
+
+
+def decrypt(line):
+    unicode_nums = [int(x) for x in line.split(' ')]
+    caesar_shift = -1
+    plain_unicode = ''.join(chr(num - index) for index, num in enumerate(unicode_nums))
+
+    # for letter in plain_unicode[11::-1]:
+    #     caesar_shift = unicode_index(letter)
+    #     if caesar_shift is not None:
+    #         break
+
+    return caesar(plain_unicode, (caesar_shift))
+
+
+
+test_line = "akwhegkwem klwane la;w,'eaw jiorl;kao;we',wal;rmawe,"
+encry = encrypt(test_line)
+print(encry)
+decoded = decrypt(encry)
+print(decoded)
